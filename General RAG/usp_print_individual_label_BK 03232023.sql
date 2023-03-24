0c66ca65e726948f0cc9d142076486246a80d3e1
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2023-03-24 16:43:08 -0500

    OK
    OK

--- General RAG/usp_print_individual_label_BK 03232023.sql
+++ General RAG/usp_print_individual_label_BK 03232023.sql
@@ -14,6 +14,117 @@ set @transmission_id = '9440980'
 
 --set @transmission_id = @82021
 --set @transmission_id = @82015
+-- DECLARE
+-- 		@hr INT,
+-- 		@err_source NVARCHAR(50),
+-- 		@err_description NVARCHAR(4000),
+-- 		@line NVARCHAR(50),			-- Line identifier (ie LA, LB, ...)
+-- 		@type NVARCHAR(50),			-- Product type (ie WB)
+-- 		@part_no NVARCHAR(50),		-- Part number
+-- 		@part_rev NVARCHAR(50),		-- Part revision
+-- 		@julian_day INT,			-- Day of the year
+-- 		@year INT,					-- Year (four digits)
+-- 		@line_code NVARCHAR(50),	-- Product type + line identifier
+-- 		@station NVARCHAR(50),	-- LINE P STATION
+-- 		@reference NVARCHAR(50),	-- External reference
+-- 		@ratio NVARCHAR(50),		-- ?
+-- 		@origen NVARCHAR(50),		-- Get Origen from CEGID
+-- 		@p1 nvarchar(50),
+-- 		@p2 nvarchar(50),
+-- 		@p3 nvarchar(50),
+-- 		@p4 nvarchar(50),
+-- 		@p5 nvarchar(50),
+-- 		@p6 nvarchar(50),
+-- 		@p7 nvarchar(50),
+-- 		@p8 nvarchar(50),
+-- 		@is_double_lid BIT;			-- Indicates whether double print is required
+
+-- 	SELECT
+-- 		@line			= t.[line],
+-- 		@type			= t.[type],
+-- 		@part_no		= t.[serial],
+-- 		@part_rev		= t.[rev],
+-- 		@julian_day		= t.[julian_day],
+-- 		@year			= t.[year],
+-- 		@line_code		= t.[type] + ' ' + t.[line],
+-- 		@reference		= RTRIM(p.ref_ext),
+-- 		@ratio			= t.ratio,
+-- 		@station        = t.station,
+-- 		@is_double_lid	= CASE WHEN t.print_count = 1 THEN 0 ELSE 1 END
+-- 	FROM APPS.dbo.pro_tms t
+--     JOIN APPS.dbo.pro_production p ON p.id = t.id_reference
+--     WHERE t.id = @transmission_id;
+-- 	--======================
+-- 	set @origen =(SELECT top 1 * FROM ufn_GetArticleOrigenCegid(@part_no,@part_rev)) --Get Origen by part_no and part_rev directly of CEGID
+-- 	select @part_no
+-- 	SELECT @part_rev
+-- 	--SELECT top 1 * FROM ufn_GetArticleOrigenCegid(@part_no,SUBSTRING(@part_rev, 1, 1))
+
+
+-- 			SELECT
+-- 		@p1 = R.patente1,
+-- 		@p2 = R.patente2,
+-- 		@p3 = R.patente3,
+-- 		@p4 = R.patente4,
+-- 		@p5 = R.patente5,
+-- 		@p6 = R.patente6,
+-- 		@p7 = R.patente7,
+-- 		@p8 = R.patente8
+-- 		FROM ufn_GetPatentsByPartNoAndRev(@part_no,@part_rev) R;
+-- 	--=======================
+-- 	DECLARE @URL NVARCHAR(MAX) = REPLACE(REPLACE('http://mxsrvapps/gtservices/printing/api/productlines/{line}/individuallabels/{type}', '{line}', @line), '{type}', @type);
+-- 	DECLARE @Object AS INT;
+-- 	DECLARE @ResponseText AS VARCHAR(8000);
+-- 	DECLARE @Body AS VARCHAR(8000) =
+-- 	'{
+-- 		"FinishGoodID": {tmId},
+-- 		"Revision": "{partRev}",
+-- 		"SerialNo": "{partNo}",
+-- 		"JulianDay": {julianDay},
+-- 		"Year": {year},
+-- 		"LineCode": "{lineCode}",
+-- 		"ExternalReference": "{reference}",
+-- 		"Ratio": "{ratio}",
+-- 		"IsDoubleLid": {isDoubleLid},
+-- 		"Station": "{station}",
+-- 		"Origen": "{origen}",
+-- 		"Patente1": "{patente1}",
+-- 		"Patente2": "{patente2}",
+-- 		"Patente3": "{patente3}",
+-- 		"Patente4": "{patente4}",
+-- 		"Patente5": "{patente5}",
+-- 		"Patente6": "{patente6}",
+-- 		"Patente7": "{patente7}",
+-- 		"Patente8": "{patente8}"
+-- 	}';
+
+-- 	SET @Body = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(@Body,
+-- 						'{tmId}', CAST(@transmission_id AS NVARCHAR(50))),
+-- 						'{partRev}', @part_rev),
+-- 						'{partNo}', @part_no),
+-- 						'{station}', @station),
+-- 						'{julianDay}', CAST(@julian_day AS NVARCHAR(50))),
+-- 						'{year}', CAST(@year AS NVARCHAR(50))),
+-- 						'{lineCode}', @line),
+-- 						'{reference}', @reference),
+-- 						-- 2022-22-04 RA: Add Origen From CEGID with ussing a function GetArticleOrigenCegid.
+-- 						'{origen}', ISNULL(NULLIF(RTRIM(@origen), ''), ' ')),
+-- 						-- 2023-23-04 RA: Add patent From CEGID and APPS with ussing a function Get[atentsByPartNoAndRev.
+-- 						'{patente1}', ISNULL(NULLIF(RTRIM(@p1), ''), ' ')),
+-- 						'{patente2}', ISNULL(NULLIF(RTRIM(@p2), ''), ' ')),
+-- 						'{patente3}', ISNULL(NULLIF(RTRIM(@p3), ''), ' ')),
+-- 						'{patente4}', ISNULL(NULLIF(RTRIM(@p4), ''), ' ')),
+-- 						'{patente5}', ISNULL(NULLIF(RTRIM(@p5), ''), ' ')),
+-- 						'{patente6}', ISNULL(NULLIF(RTRIM(@p6), ''), ' ')),
+-- 						'{patente7}', ISNULL(NULLIF(RTRIM(@p7), ''), ' ')),
+-- 						'{patente8}', ISNULL(NULLIF(RTRIM(@p8), ''), ' ')),
+-- 						-- 2022-03-10 MV: Value might have blank space to the right affecting the label output.
+-- 						'{ratio}', ISNULL(NULLIF(RTRIM(@ratio), ''), 'NA')),
+-- 						'{isDoubleLid}', CASE WHEN @is_double_lid = 1 THEN 'true' ELSE 'false' END);
+--                        SELECT @Body
+
+
+
 DECLARE
 		@hr INT,
 		@err_source NVARCHAR(50),
@@ -28,16 +139,18 @@ DECLARE
 		@station NVARCHAR(50),	-- LINE P STATION
 		@reference NVARCHAR(50),	-- External reference
 		@ratio NVARCHAR(50),		-- ?
-		@origen NVARCHAR(50),		-- Get Origen from CEGID
-		@p1 nvarchar(50),
-		@p2 nvarchar(50),
-		@p3 nvarchar(50),
-		@p4 nvarchar(50),
-		@p5 nvarchar(50),
-		@p6 nvarchar(50),
-		@p7 nvarchar(50),
-		@p8 nvarchar(50),
+		@origen NVARCHAR(50),		-- Get Origen from CEGID 
+		@p1 nvarchar(50),			-- Get Patent from CEGID
+		@p2 nvarchar(50),			-- Get Patent from CEGID
+		@p3 nvarchar(50),			-- Get Patent from CEGID
+		@p4 nvarchar(50),			-- Get Patent from CEGID
+		@p5 nvarchar(50),			-- Get Patent from CEGID
+		@p6 nvarchar(50),			-- Get Patent from CEGID
+		@p7 nvarchar(50),			-- Get Patent from CEGID
+		@p8 nvarchar(50),			-- Get Patent from CEGID
+		--@is_ME_FL BIT,
 		@is_double_lid BIT;			-- Indicates whether double print is required
+		
 
 	SELECT
 		@line			= t.[line],
@@ -54,13 +167,9 @@ DECLARE
 	FROM APPS.dbo.pro_tms t
     JOIN APPS.dbo.pro_production p ON p.id = t.id_reference
     WHERE t.id = @transmission_id;
-	--======================
-	set @origen =(SELECT top 1 * FROM ufn_GetArticleOrigenCegid(@part_no,@part_rev)) --Get Origen by part_no and part_rev directly of CEGID
-	select @part_no
-	SELECT @part_rev
-	--SELECT top 1 * FROM ufn_GetArticleOrigenCegid(@part_no,SUBSTRING(@part_rev, 1, 1))
-
-
+	--set @origen =(SELECT * FROM ufn_GetArticleOrigenCegid(@part_no,@part_rev)) --Get Origen by part_no and part_rev directly of CEGID
+	--set @origen =(SELECT top 1 * FROM ufn_GetArticleOrigenCegid(@part_no,SUBSTRING(@part_rev, 1, 1))) --Get Origen by part_no and part_rev directly of CEGID
+	
 			SELECT
 		@p1 = R.patente1,
 		@p2 = R.patente2,
@@ -71,7 +180,23 @@ DECLARE
 		@p7 = R.patente7,
 		@p8 = R.patente8
 		FROM ufn_GetPatentsByPartNoAndRev(@part_no,@part_rev) R;
-	--=======================
+
+--=================================================================================
+/*Esto es un mal truco, ya que dezconozco desde donde se escribe el valor type en la tabla APPS.dbo.pro_tms
+asi que el truco actuda de manera que si no detecta nincuna patente existente enconces deja el tipo original, en caso de que detecte alguna patente,
+entonces la etiqueta es de tipo ME FL*/
+		declare @is_ME_FL BIT;
+		IF COALESCE(@p1, '') = '' 
+BEGIN
+    SET @is_ME_FL = 0;
+END
+ELSE
+BEGIN
+    SET @is_ME_FL = 1;
+	SET @type = 'ME FL'
+END;
+--=================================================================================
+	SELECT @type
 	DECLARE @URL NVARCHAR(MAX) = REPLACE(REPLACE('http://mxsrvapps/gtservices/printing/api/productlines/{line}/individuallabels/{type}', '{line}', @line), '{type}', @type);
 	DECLARE @Object AS INT;
 	DECLARE @ResponseText AS VARCHAR(8000);
@@ -86,7 +211,7 @@ DECLARE
 		"ExternalReference": "{reference}",
 		"Ratio": "{ratio}",
 		"IsDoubleLid": {isDoubleLid},
-		"Station": "{station}",
+		"Station": {station},
 		"Origen": "{origen}",
 		"Patente1": "{patente1}",
 		"Patente2": "{patente2}",
@@ -98,6 +223,7 @@ DECLARE
 		"Patente8": "{patente8}"
 	}';
 
+
 	SET @Body = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(@Body,
 						'{tmId}', CAST(@transmission_id AS NVARCHAR(50))),
 						'{partRev}', @part_rev),
@@ -107,9 +233,9 @@ DECLARE
 						'{year}', CAST(@year AS NVARCHAR(50))),
 						'{lineCode}', @line),
 						'{reference}', @reference),
-						-- 2022-22-04 RA: Add Origen From CEGID with ussing a function GetArticleOrigenCegid.
+						-- 2022-22-04 RA: Added Origen From CEGID with ussing a function GetArticleOrigenCegid.
 						'{origen}', ISNULL(NULLIF(RTRIM(@origen), ''), ' ')),
-						-- 2023-23-04 RA: Add patent From CEGID and APPS with ussing a function Get[atentsByPartNoAndRev.
+						-- 2023-23-04 RA: Added patent From CEGID and APPS with ussing a function Get[atentsByPartNoAndRev.
 						'{patente1}', ISNULL(NULLIF(RTRIM(@p1), ''), ' ')),
 						'{patente2}', ISNULL(NULLIF(RTRIM(@p2), ''), ' ')),
 						'{patente3}', ISNULL(NULLIF(RTRIM(@p3), ''), ' ')),
@@ -121,4 +247,5 @@ DECLARE
 						-- 2022-03-10 MV: Value might have blank space to the right affecting the label output.
 						'{ratio}', ISNULL(NULLIF(RTRIM(@ratio), ''), 'NA')),
 						'{isDoubleLid}', CASE WHEN @is_double_lid = 1 THEN 'true' ELSE 'false' END);
-                       SELECT @Body
+
+						SELECT @Body
