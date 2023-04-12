66bec64ce1b798971d45aab7d0e8262a43a27b96
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2023-04-12 13:31:38 -0500

    Update

--- General/contrasenas kiosko.sql
+++ General/contrasenas kiosko.sql
@@ -5,11 +5,3 @@
 --select EMPLEADO, NOMBRE, PATERNO, MATERNO, PWDKIOSCO from EMP_RH where EMPLEADO>=7899 AND EMPLEADO < 7912
 
 --============================================================================================================================================
--- SELECT * from EMP_RH WHERE EMPLEADO = '5785'
--- SELECT * from EMP_RH WHERE EMPLEADO = '5787'
-
-SELECT * from EMP_RH WHERE EMPLEADO in('5787','5785') 
-
---SELECT NOMBRE, PATERNO, MATERNO, IMSS, RFC from EMP_RH WHERE EMPLEADO in('5787','5785') 
-
---select * from MXSRVADMIN.NETGT.dbo.EMP_RH
