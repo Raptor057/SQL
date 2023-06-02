f82b5855627ff5f169908342f0f6e180262906fc
Rogelio Arriaga <rogelioarriaga@live.com.mx>
2023-06-02 09:58:20 -0500

    Update

--- General/contrasenas kiosko.sql
+++ General/contrasenas kiosko.sql
@@ -1,7 +1,7 @@
 --============================================================================================================================================
 --======================    Esto es lo que se tiene que ejecutar, lo de abajo es para buscar a un empleado con su numero    ==================
 
---update emp_rh set PWDKIOSCO=RIGHT('0000' + CAST(ABS(CHECKSUM(NewId())) % 10000 AS VARCHAR(4)), 4) where EMPLEADO>=7899 AND EMPLEADO < 7912
---select EMPLEADO, NOMBRE, PATERNO, MATERNO, PWDKIOSCO from EMP_RH where EMPLEADO>=7899 AND EMPLEADO < 7912
+update EMP_RH set PWDKIOSCO=RIGHT('0000' + CAST(ABS(CHECKSUM(NewId())) % 10000 AS VARCHAR(4)), 4) where EMPLEADO>=5788 AND EMPLEADO < 5793
+select EMPLEADO, NOMBRE, PATERNO, MATERNO, PWDKIOSCO from EMP_RH where EMPLEADO>=5788 AND EMPLEADO < 5793
 
 --============================================================================================================================================
