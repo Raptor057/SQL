86ba67719ca63440bba5f7fa4c36889d727b2d1f
Rogelio Arriaga <rogelioarriaga@live.com.mx>
2024-01-04 08:07:14 -0600

    Update

--- General RAG/Sin Dev/Recuperar Etiquetas retornadas por cambio de modelo X linea.sql
+++ General RAG/Sin Dev/Recuperar Etiquetas retornadas por cambio de modelo X linea.sql
@@ -11,16 +11,18 @@ SET @EtiNo = ''
 SET @ComponentNo = ''
 
 SELECT TOP 1000 * FROM [gtt].[dbo].[PointOfUseEtis]
-WHERE PointOfUseCode LIKE (CONCAT('%',@Line,'%')) --OR EtiNo = @EtiNo OR @ComponentNo = @ComponentNo
+WHERE PointOfUseCode LIKE (CONCAT('%',@Line,'%')) --AND EtiNo = @EtiNo AND @ComponentNo = @ComponentNo
 -- AND UtcUsageTime IS NULL AND UtcExpirationTime IS NULL                          -- 1) Descomentar esta linea para ver las etiquetas cargadas pero no usadas
 -- AND UtcUsageTime IS NOT NULL AND UtcExpirationTime IS NULL                      -- 2) Descomentar esta linea para ver las etiquetas usadas en momento
 -- AND UtcUsageTime is NULL AND UtcExpirationTime IS NOT NULL and IsDepleted != 1  -- 3) Descomentar esta linea para ver las etiquetas descargadas mas no consumidas ni usadas (Retorno sin usar)
 -- AND UtcExpirationTime IS NOT NULL and IsDepleted != 1                           -- 4) Descomentar esta linea para ver las etiquetas descargadas mas no consumidas ni usadas (Retorno en general)
 -- AND UtcExpirationTime IS NOT NULL and IsDepleted != 0                           -- 5) Descomentar esta linea para ver las etiquetas ya consumidas.
+--AND UtcExpirationTime = '' -- Cuando ya se tenga la fecha del tiempo de descarga ponerlo aqui para ver realmente cuantos componentes se tendrian que haber descargado
 -- ORDER BY PointOfUseCode ASC,UtcEffectiveTime DESC, UtcUsageTime DESC --Descomentar esto solo si lo quires ver organizado por Linea -> fecha de carga -> fecha de uso 
  ORDER BY UtcExpirationTime DESC
 
 -- Aqui tienes que poner la fecha de descarga, por lo general todos los componentes tendran igual la fecha de descarga esto lo puedes ver descomentando el punto 4 del query 
 --Descomentar esto para hacer ese cambio
---UPDATE [gtt].[dbo].[PointOfUseEtis] SET UtcExpirationTime = NULL WHERE UtcExpirationTime = '' AND PointOfUseCode LIKE (CONCAT('',REPLACE(@Line, 'L', ''),''))
+-- UPDATE [gtt].[dbo].[PointOfUseEtis] SET UtcExpirationTime = NULL WHERE UtcExpirationTime = '' AND PointOfUseCode LIKE (CONCAT('',REPLACE(@Line, 'L', ''),''))
 
+SELECT PointOfUseCode,EtiNo,ComponentNo,UtcExpirationTime FROM [gtt].[dbo].[PointOfUseEtis] WHERE IsDepleted = 0 GROUP BY PointOfUseCode,EtiNo,ComponentNo,UtcExpirationTime ORDER BY PointOfUseCode ASC, UtcExpirationTime DESC
